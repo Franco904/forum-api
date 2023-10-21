9f4be7db4d1e4ac83f14ab5d0059a8bc9f94e9b9
Franco Tavares <francostavares2003@gmail.com>
2023-10-21 06:34:26 -0300

    style: format code

--- src/main/kotlin/br/com/alura/forumapi/ForumApiApplication.kt
+++ src/main/kotlin/br/com/alura/forumapi/ForumApiApplication.kt
@@ -7,5 +7,5 @@ import org.springframework.boot.runApplication
 class ForumApiApplication
 
 fun main(args: Array<String>) {
-	runApplication<ForumApiApplication>(*args)
+    runApplication<ForumApiApplication>(*args)
 }
