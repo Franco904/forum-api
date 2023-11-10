dfaae6e2a12a58404b9f8506947679b2febde506
Franco Tavares <francostavares2003@gmail.com>
2023-11-10 11:05:23 -0300

    style: fix type error

--- src/main/kotlin/br/com/alura/forumapi/exception/ExceptionHandler.kt
+++ src/main/kotlin/br/com/alura/forumapi/exception/ExceptionHandler.kt
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 class ExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException::class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    fun handleBadResquestError(
+    fun handleBadRequestError(
         exception: MethodArgumentNotValidException,
         request: HttpServletRequest,
     ): GetErrorDto {
