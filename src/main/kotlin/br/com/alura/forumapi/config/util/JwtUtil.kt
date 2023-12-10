fa6eff744478736e61bc1f4de355b534525f0351
Franco Saravia Tavares <francostavares2003@gmail.com>
2023-12-10 10:59:01 -0300

    feat: add JWT generator class

---
+++ src/main/kotlin/br/com/alura/forumapi/config/util/JwtUtil.kt
@@ -0,0 +1,23 @@
+package br.com.alura.forumapi.config.util
+
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import java.util.*
+
+@Component
+object JwtUtil {
+    private const val EXPIRATION_IN_MILLIS: Long = 60000 // 1 min
+    @Value("\${jwt.secret}")
+    private lateinit var secret: String
+
+    fun generateToken(username: String): String? {
+        return Jwts
+            .builder()
+            .setSubject(username)
+            .setExpiration(Date(System.currentTimeMillis().plus(EXPIRATION_IN_MILLIS)))
+            .signWith(SignatureAlgorithm.HS256, secret.toByteArray())
+            .compact()
+    }
+}
