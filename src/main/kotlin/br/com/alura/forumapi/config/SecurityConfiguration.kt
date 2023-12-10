f61a04c6272d7650bf55fc9eebeac40f84f5900d
Franco Saravia Tavares <francostavares2003@gmail.com>
2023-12-10 14:17:10 -0300

    feat: add JwtLoginFilter and set it up in SecurityConfiguration

--- src/main/kotlin/br/com/alura/forumapi/config/SecurityConfiguration.kt
+++ src/main/kotlin/br/com/alura/forumapi/config/SecurityConfiguration.kt
@@ -1,5 +1,7 @@
 package br.com.alura.forumapi.config
 
+import br.com.alura.forumapi.security.filter.JwtLoginFilter
+import br.com.alura.forumapi.security.util.JwtUtil
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
@@ -8,10 +10,13 @@ import org.springframework.security.config.annotation.web.invoke
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfiguration {
+class SecurityConfiguration(
+    private val jwtUtil: JwtUtil,
+) {
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         http {
@@ -22,6 +27,10 @@ class SecurityConfiguration {
                 // Only accept requests sent by authenticated users
                 authorize(anyRequest, authenticated)
             }
+
+            // Configure request filters
+            addFilterBefore<UsernamePasswordAuthenticationFilter>(JwtLoginFilter(authenticationManager, jwtUtil))
+
             // Disable state saves between requests when logged in a user session
             sessionManagement {
                 sessionCreationPolicy = SessionCreationPolicy.STATELESS
